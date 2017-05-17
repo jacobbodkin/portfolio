63e327d4bbd338c360fc4e96d3b6711af4fc63c1
Jacob Bodkin <bodkin.j@gmail.com>
2017-05-17 13:11:42 -0500

    changed about page to say i built site

--- src/components/About.js
+++ src/components/About.js
@@ -38,11 +38,11 @@ class About extends Component {
             </div>
           </div>
           <div className="descText col-md-7 col-12">
-            <p>I grew up in Texas loving to climb anything and everything. Trees were good, chossy cliffs worked, but the mountains are what truly fed the craving. Going to school for engineering never really felt right, but climbing did, so every weekend and free moment was devoted to it. Climbing led to more climbing led to bigger climbing trips. I picked up a camera to remember the epics (ok, maybe to scare my mom a little bit once I got home ) and soon climbing trips were as much about the photography as they were about the climbing. Like climbing, I could express myself visually and physically through photography in a way that felt natural, a medium that I could immerse myself in. After years working a nine to five and waiting for every holiday and weekend, I struck out on my own to make my passions my life.</p>
-            <br/>
-            <p>Austin, TX based, feel free to give me a call or shoot me an email. I'm always happy to talk shop or about a project you are excited about.</p>
-            <br/>
-            <p>If you are into architecture, see my architectural photo work at <a className="descText" href="http://jacobbodkinphoto.com" target="_blank">jacobbodkinphoto.com</a>.</p>
+            <p>I grew up in Texas loving to climb anything and everything. Trees were good, chossy cliffs worked, but the mountains are what truly fed the craving. Climbing felt natural, so every weekend and free moment was devoted to it. Climbing led to more climbing led to bigger climbing trips. I picked up a camera to remember the epics (ok, maybe to scare my mom a little bit once I got home ) and soon climbing trips were as much about the photography as they were about the climbing. Like climbing, I could express myself visually and physically through photography in a way that felt natural, a medium that I could immerse myself in.</p>
+
+            <p>Recently, I've immersed myself in coding developing mobile and web apps. I designed and developed this site from scratch using React and will constantly be a work-in-progress.</p>
+
+            <p>Austin, TX based, feel free to give me a call or shoot me an email. I'm always happy to talk shop or about aany project you are excited about.</p>
           </div>
         </div>
 
