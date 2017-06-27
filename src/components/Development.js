30a793714943b8736b4c1812c6a5f68dc7ab50c0
Jacob Bodkin <bodkin.j@gmail.com>
2017-06-27 15:19:13 -0500

    optimized image sizes

--- src/components/Development.js
+++ src/components/Development.js
@@ -20,7 +20,7 @@ class Development extends Component {
 
           <a href="https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=1233304561&mt=8" className="col-12 col-md-6 col-lg-4 wtp row" target="_blank">
             <div className="figure col-12 light">
-              <img src="./images/wethepeople.png" alt="Thumb" />
+              <img src="./images/wethepeople.jpg" alt="Thumb" />
               <div className="figcaption">
                 <div className="lightTitle">We the People</div>
               </div>
@@ -29,7 +29,7 @@ class Development extends Component {
 
           <Link to="/" className="col-12 col-md-6 col-lg-4 row">
             <div className="figure col-12">
-              <img src="./images/jbp.png" alt="Thumb" />
+              <img src="./images/jbp.jpg" alt="Thumb" />
               <div className="figcaption">
                 <div>Jacob Bodkin Photo</div>
               </div>
@@ -38,7 +38,7 @@ class Development extends Component {
 
           <a href="https://supply-the-love.herokuapp.com/" className="col-12 col-md-6 col-lg-4 row" target="_blank">
             <div className="figure col-12 ">
-              <img src="./images/slv.png" alt="Thumb" />
+              <img src="./images/slv.jpg" alt="Thumb" />
               <div className="figcaption">
                 <div>Supply the Love</div>
               </div>
@@ -47,7 +47,7 @@ class Development extends Component {
 
           <a href="https://jacobbodkin.github.io/blackjack-game/" className="col-12 col-md-6 col-lg-4 row" target="_blank">
             <div className="figure col-12">
-              <img src="./images/blackjack.png" alt="Thumb" />
+              <img src="./images/blackjack.jpg" alt="Thumb" />
               <div className="figcaption">
                 <div>Blackjack</div>
               </div>
